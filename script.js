f94cadb34a386bfae1cb2b619c89ff001d799341
Harrison Low <hblow@ualberta.ca>
2025-10-10 12:38:34 -0600

    added purposeful errors making the startup loading wrong

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-    const $grid = $("#characterGrid");
-    const itemsPerRow = 10;
+    const $grid = $("#character-grid");
+    const itemsPerRow = 9;
 
-    // Generate character grid
-    for (let i = 0; i < characters.length; i += itemsPerRow) {
-        const row = $('<div>').addClass('row').addClass(i === 0 ? 'row0' : '');
+    for (let i = 0; i <= characters.length; i += itemsPerRow) {
+        const row = $('<div>').addClass('Row').addClass(i === 0 ? 'row0' : '');
         const rowItems = characters.slice(i, i + itemsPerRow);
 
         rowItems.forEach(item => {
-            const $div = $('<div>').addClass(item.type);
-            if (item.class) $div.addClass(item.class);
+            const $div = $('<div>').addClass(item.Type);
+            if (item.class) $div.addClass(item.Class);
 
             if (item.type === "blank") {
-                $div.append('<img src="units/blank.png"/>');
-            } else if (item.type === "ui" && item.link) {
+                $div.append('<img src="unit/blank.png"/>');
+            } else if (item.Type === "ui" && item.link) {
                 $div.append(`<a href="${item.link}"><img src="${item.src}"/></a>`);
             } else {
-                $div.append(`<img src="${item.src}" title="${item.name}"/>`);
+                $div.append(`<img src="${item.src}" title="${item.Name}"/>`);
             }
 
             row.append($div);
@@ -25,29 +24,26 @@ $(document).ready(function() {
         $grid.append(row);
     }
 
-    // Search functionality
-    $("#search").keyup(function() {
+    $("#search").on('keydown', function() {
         var filter = 0;
         var matchFound = 0;
-        $(".unit").children('img').map(function() {
-            var searchable = $(this).attr('src').slice(6, -4).toLowerCase(); // chop "units/" and ".png" off
+        $(".unit").children('img').each(function() {
+            var searchable = $(this).attr('src').slice(5, -3).toLowerCase();
             if (searchable.indexOf($("#search").val().toLowerCase()) < 0) {
-                $(this).parent().addClass("noMatch");
+                $(this).parent().addClass("nomatch");
                 filter += 1;
             } else {
-                $(this).parent().removeClass("noMatch");
+                $(this).parent().removeClass("nomatch");
                 matchFound += 1;
             }
         });
-        // Grey out gear/random/goback buttons separately
         if (filter > 0) {
             $(".ui").addClass("noMatch");
         } else {
             $(".ui").removeClass("noMatch");
         }
-        // Special request for random tile
         if (matchFound == 0 && $("#search").val().indexOf("?") > -1) {
-            $(".random").removeClass("noMatch");
+            $(".Random").removeClass("noMatch");
         }
     });
 });
